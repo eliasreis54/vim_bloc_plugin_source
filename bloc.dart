ec02dcfdb1e24d0155ac34dfe1e175ef4b1d13c0
Elias Reis <eliasreis@localhost.localdomain>
2022-01-02 21:48:00 -0300

    bloc

---
+++ bloc.dart
@@ -0,0 +1,13 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part '<rename>_event.dart';
+part '<rename>_state.dart';
+
+class <rename>Bloc extends Bloc<<rename>Event, <rename>State> {
+  <rename>Bloc() : super(<rename>Initial()) {
+    on<<rename>Event>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
