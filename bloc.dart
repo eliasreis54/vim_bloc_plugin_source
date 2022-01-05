547500a499b998b2b28d3b9c95a7f777b3e4f51c
Elias Reis <eliasreis54@gmail.com>
2022-01-05 17:03:00 -0300

    use factory on initial state

--- bloc.dart
+++ bloc.dart
@@ -5,7 +5,7 @@ part '<rename_file>_event.dart';
 part '<rename_file>_state.dart';
 
 class <rename>Bloc extends Bloc<<rename>Event, <rename>State> {
-  <rename>Bloc() : super(<rename>Initial()) {
+  <rename>Bloc() : super(<rename>State.initial()) {
     on<<rename>Event>((event, emit) {
       // TODO: implement event handler
     });
